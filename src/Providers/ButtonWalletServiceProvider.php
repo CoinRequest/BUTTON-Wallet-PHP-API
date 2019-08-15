ce3b009a12b14d74c7191a5c28dfc7ab84a07572
Paul Wijnberg <contact@wijnbergdevelopments.nl>
2019-08-15 22:10:12 +0200

    Added ServiceProvider for Laravel

---
+++ src/Providers/ButtonWalletServiceProvider.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace ButtonWallet\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class ButtonWalletServiceProvider extends ServiceProvider
+{
+
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+
+
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+
+
+    }
+
+}
